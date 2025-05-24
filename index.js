6fbf6381e4bd2df309eebf26a43153f92a4ef11b
tejas-exe <68724559+tejas-exe@users.noreply.github.com>
2025-05-24 13:02:41 +0530

    basic server
    ->basic server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const 
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
